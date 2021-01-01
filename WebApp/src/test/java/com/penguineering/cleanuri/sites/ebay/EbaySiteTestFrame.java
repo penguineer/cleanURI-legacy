e51c8665b56655839b7883e3597d16faebf0c62f
David Kilias <david.kilias@gmail.com>
2021-01-01 23:43:35 +0100

    add ebay test

---
+++ WebApp/src/test/java/com/penguineering/cleanuri/sites/ebay/EbaySiteTestFrame.java
@@ -0,0 +1,43 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.penguineering.cleanuri.sites.ebay;
+
+import java.net.URI;
+import java.util.Map;
+
+import com.penguineering.cleanuri.Site;
+import com.penguineering.cleanuri.api.Decorator;
+import com.penguineering.cleanuri.api.ExtractorException;
+import com.penguineering.cleanuri.api.Metakey;
+import com.penguineering.cleanuri.decorators.DokuwikiDecorator;
+import com.penguineering.cleanuri.decorators.JsonDecorator;
+
+public class EbaySiteTestFrame {
+
+	public static void main(String args[]) throws ExtractorException {
+		final URI uri = URI
+				.create("https://www.ebay.de/itm/Raspberry-Pi-4-Computer-Modell-B-4GB-RAM-Light-Starter-Kit-weis-Bundle/113903558395?epid=7035054216&hash=item1a852e5efb:g:gtAAAOSwVztf7lwx");
+
+		final Site site = EbaySite.getInstance();
+
+		URI href = site.getCanonizer().canonize(uri);
+		Map<Metakey, String> meta = site.getExtractor().extractMetadata(href);
+
+		Decorator decorator = new DokuwikiDecorator();
+		System.out.println(decorator.decorate(href, meta));
+		
+		decorator = new JsonDecorator();
+		System.out.println(decorator.decorate(href, meta));
+	}
+}
